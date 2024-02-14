9e57a4c96aa8a6fe45d71cc3c57d9903eef223b5
david <david.haslauer@yahoo.com>
2024-02-14 15:44:42 +0100

    VoidItem Crafting recipe

--- src/main/java/com/example/examplemod/items/VoidItem.java
+++ src/main/java/com/example/examplemod/items/VoidItem.java
@@ -33,6 +33,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -77,24 +78,43 @@ public class VoidItem extends Item{
     }
 
     @Override
+    public boolean isCorrectToolForDrops(BlockState pBlock) {
+        return true;
+    }
+    @Override
     public InteractionResult useOn(UseOnContext pContext) {
 
         Level level = pContext.getLevel();
-        BlockPos pos = pContext.getClickedPos();
-        level.destroyBlock(pos,true);
+        BlockPos bPos = pContext.getClickedPos();
+        level.destroyBlock(bPos,true);
 
+        Vec3 pos = bPos.getCenter();
+
+        for(int i = 0; i < 20; i++) {
+            level.addParticle(ParticleTypes.ASH, pos.x + (Math.random() * 2) - 1, pos.y + (Math.random() * 2) - 1, pos.z + (Math.random() * 2) - 1, 0, 0, 0);
+            level.addParticle(ParticleTypes.ENCHANT, pos.x + (Math.random() * 2) - 1, pos.y + (Math.random() * 2) - 1, pos.z + (Math.random() * 2) - 1, 0, 0, 0);
+        }
 
         return super.useOn(pContext);
     }
 
     @Override
-    public boolean onBlockStartBreak(ItemStack itemstack, BlockPos pos, Player player) {
+    public float getDestroySpeed(ItemStack pStack, BlockState pState) {
+     return 9999;
+    }
 
-        player.sendSystemMessage(Component.literal("found"));
-        player.getCommandSenderWorld()
-        .destroyBlock(pos,true);
+    @Override
+    public boolean onBlockStartBreak(ItemStack itemstack, BlockPos bPos, Player player) {
 
-        return super.onBlockStartBreak(itemstack, pos, player);
+        Vec3 pos = bPos.getCenter();
+        Level level = player.getCommandSenderWorld();
+
+        for(int i = 0; i < 20; i++) {
+            level.addParticle(ParticleTypes.ASH, pos.x + (Math.random() * 2) - 1, pos.y + (Math.random() * 2) - 1, pos.z + (Math.random() * 2) - 1, 0, 0, 0);
+            level.addParticle(ParticleTypes.ENCHANT, pos.x + (Math.random() * 2) - 1, pos.y + (Math.random() * 2) - 1, pos.z + (Math.random() * 2) - 1, 0, 0, 0);
+        }
+
+        return super.onBlockStartBreak(itemstack, bPos, player);
     }
 
     @Override
@@ -103,17 +123,33 @@ public class VoidItem extends Item{
 
         player.setHealth(20);
         entity.kill();
+        Vec3 pos = entity.getEyePosition();
+        BlockPos bPos = player.getOnPos();
+        Level level = player.getCommandSenderWorld();
+        level.playSound(player,bPos,SoundEvents.TOTEM_USE,SoundSource.PLAYERS);
 
+        for(int i = 0; i < 20; i++) {
+            level.addParticle(ParticleTypes.ASH, pos.x + (Math.random() * 2) - 1, pos.y + (Math.random() * 2) - 1, pos.z + (Math.random() * 2) - 1, 0, 0, 0);
+            level.addParticle(ParticleTypes.ENCHANT, pos.x + (Math.random() * 2) - 1, pos.y + (Math.random() * 2) - 1, pos.z + (Math.random() * 2) - 1, 0, 0, 0);
+        }
         return super.onLeftClickEntity(stack, player, entity);
     }
 
     @Override
-    public InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
+    public InteractionResult interactLivingEntity(ItemStack pStack, Player player, LivingEntity entity, InteractionHand pUsedHand) {
 
-        pPlayer.setHealth(20);
-        pInteractionTarget.kill();
+        player.setHealth(20);
+        entity.kill();
+        Vec3 pos = entity.getEyePosition();
+        BlockPos bPos = player.getOnPos();
+        Level level = player.getCommandSenderWorld();
+        level.playSound(player,bPos,SoundEvents.TOTEM_USE,SoundSource.PLAYERS);
 
-        return super.interactLivingEntity(pStack, pPlayer, pInteractionTarget, pUsedHand);
+        for(int i = 0; i < 20; i++) {
+            level.addParticle(ParticleTypes.ASH, pos.x + (Math.random() * 2) - 1, pos.y + (Math.random() * 2) - 1, pos.z + (Math.random() * 2) - 1, 0, 0, 0);
+            level.addParticle(ParticleTypes.ENCHANT, pos.x + (Math.random() * 2) - 1, pos.y + (Math.random() * 2) - 1, pos.z + (Math.random() * 2) - 1, 0, 0, 0);
+        }
+        return super.interactLivingEntity(pStack, player, entity, pUsedHand);
     }
 
     @Override
@@ -122,7 +158,7 @@ public class VoidItem extends Item{
         if(pEntity instanceof Player){
 
             Player player = (Player) pEntity;
-            player.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, 2,2));
+            player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 2,2));
             player.addEffect(new MobEffectInstance(MobEffects.LUCK, 2,2));
 
 
